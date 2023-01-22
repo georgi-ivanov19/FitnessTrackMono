4cb90d18bc85c93ce03d90c424096c03d5617ac1
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-01-22 14:11:23 +0000

    feat: update user model and scaffold identity pages

--- Server/Models/ApplicationUser.cs
+++ Server/Models/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace FitnessTrackMono.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = String.Empty;
         public List<Measurement> Measurements { get; set; } = new List<Measurement>();
         public List<Meal> Meals { get; set; } = new List<Meal>();
         public List<Routine> Routines { get; set; } = new List<Routine>();
