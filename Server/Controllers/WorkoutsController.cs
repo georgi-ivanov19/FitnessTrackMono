68f10271edbb5c0d2f84b2bffb750777906333d5
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-01-26 18:16:07 +0000

    fix: apply date and notes changes to workout

--- Server/Controllers/WorkoutsController.cs
+++ Server/Controllers/WorkoutsController.cs
@@ -9,84 +9,86 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FitnessTrackMono.Server.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class WorkoutsController : ControllerBase
-  {
-
-    private readonly ApplicationDbContext _context;
-    private readonly UserManager<ApplicationUser> _userManager;
-
-    public WorkoutsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkoutsController : ControllerBase
     {
-      _context = context;
-      _userManager = userManager;
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public WorkoutsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Workout>>> GetWorkouts()
+        {
+            var user = await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+            var userWorkouts = _context.Workouts.Where(w => w.ApplicationUserId == user.Id).ToList();
+            return Ok(userWorkouts);
+        }
+
+        [HttpGet("GetWorkout/{id}")]
+        public async Task<ActionResult<Workout>> GetSingleWorkout(int id)
+        {
+            var workout = await _context.Workouts.FirstOrDefaultAsync(w => w.Id == id);
+            if (workout == null)
+            {
+                return NotFound("Workout not found");
+            }
+            return Ok(workout);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Workout>> CreateWorkout(Workout workout)
+        {
+            var user = await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            workout.ApplicationUserId = user.Id;
+            _context.Workouts.Add(workout);
+            await _context.SaveChangesAsync();
+
+            return Ok(workout);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Workout>> UpdateWorkout(Workout workout, int id)
+        {
+            var dbWorkout = await _context.Workouts.FirstOrDefaultAsync(w => w.Id == id);
+            if (dbWorkout == null)
+            {
+                return NotFound("Workout Not Found");
+            }
+            dbWorkout.Name = workout.Name;
+            dbWorkout.DayOfWeek = workout.DayOfWeek;
+            dbWorkout.Notes = workout.Notes;
+            dbWorkout.DateLastCompleted = workout.DateLastCompleted;
+            await _context.SaveChangesAsync();
+
+            return Ok(dbWorkout);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteWorkout(int id)
+        {
+            var dbWorkout = await _context.Workouts.FirstOrDefaultAsync(w => w.Id == id);
+            if (dbWorkout == null)
+            {
+                return NotFound("Workout Not Found");
+            }
+
+            _context.Workouts.Remove(dbWorkout);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
-
-    [HttpGet]
-    public async Task<ActionResult<List<Workout>>> GetWorkouts()
-    {
-      var user = await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
-      if (user == null)
-      {
-        return NotFound("User not found");
-      }
-      var userWorkouts = _context.Workouts.Where(w => w.ApplicationUserId== user.Id).ToList();
-      return Ok(userWorkouts);
-    }
-
-    [HttpGet("GetWorkout/{id}")]
-    public async Task<ActionResult<Workout>> GetSingleWorkout(int id)
-    {
-      var workout = await _context.Workouts.FirstOrDefaultAsync(w => w.Id == id);
-      if (workout == null)
-      {
-        return NotFound("Workout not found");
-      }
-      return Ok(workout);
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<Workout>> CreateWorkout(Workout workout)
-    {
-      var user = await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
-      workout.ApplicationUserId = user.Id;
-      _context.Workouts.Add(workout);
-      await _context.SaveChangesAsync();
-
-      return Ok(workout);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<ActionResult<Workout>> UpdateWorkout(Workout workout, int id)
-    {
-      var dbWorkout = await _context.Workouts.FirstOrDefaultAsync(w => w.Id == id);
-      if (dbWorkout == null)
-      {
-        return NotFound("Workout Not Found");
-      }
-      dbWorkout.Name = workout.Name;
-      dbWorkout.DayOfWeek = workout.DayOfWeek;
-      await _context.SaveChangesAsync();
-
-      return Ok(dbWorkout);
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteWorkout(int id)
-    {
-      var dbWorkout = await _context.Workouts.FirstOrDefaultAsync(w => w.Id == id);
-      if (dbWorkout == null)
-      {
-        return NotFound("Workout Not Found");
-      }
-
-      _context.Workouts.Remove(dbWorkout);
-      await _context.SaveChangesAsync();
-
-      return Ok();
-    }
-  }
 }
 
 
