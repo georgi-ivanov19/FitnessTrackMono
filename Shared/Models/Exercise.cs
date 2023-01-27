b195dbcdfda5ec1b8a5180b7b3b5ffee854d8cf0
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-01-27 19:07:27 +0000

    fix: remove abundant field

--- Shared/Models/Exercise.cs
+++ Shared/Models/Exercise.cs
@@ -12,7 +12,6 @@ namespace FitnessTrackMono.Shared.Models
         public string Name { get; set; } = string.Empty;
         public int WorkoutId { get; set; }
         public int DefaultNumberOfSets { get; set; }
-        public int ActualNumberOfSets { get; set; }
         public string TargetMuscle { get; set; } = string.Empty;
         public List<ExerciseSet> ExerciseSets { get; set; } = new List<ExerciseSet>();
     }
