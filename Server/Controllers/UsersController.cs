296a9f6029f017e9bb8b94a08d6aeeb27b3aa2a1
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-03-26 23:18:50 +0100

    feat: add endpoint for all user IDs

---
+++ Server/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using FitnessTrackMono.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTrackMono.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<string>> GetUserIds()
+        {
+            return await _context.Users.Select(u => u.Id).ToListAsync();
+        }
+    }
+}
